7ed92c4369856596dc0c3eb58fa214a74eee61f8
pjs21703 <sprakhar.janardan.eee21@itbhu.ac.in>
2023-06-22 18:29:12 +0530

    added other files

---
+++ client/src/setupTests.js
@@ -0,0 +1,5 @@
+// Jest as being the tool to create the testing environment. 
+// It provides the context in which to render components for testing,
+//  and creates the test suites and test blocks.
+
+import '@testing-library/jest-dom';
