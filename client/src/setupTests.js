663839e848be12b46f5a87ad488e92ab98220fea
pjs21703 <sprakhar.janardan.eee21@itbhu.ac.in>
2023-06-22 19:57:21 +0530

    added web vitals and tests

---
+++ client/src/setupTests.js
@@ -0,0 +1,6 @@
+// By testing code, developers can ensure it 
+// meets the requirements and specifications of the application and identify 
+// areas that may need improvement or optimization. 
+
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+import '@testing-library/jest-dom';
